20c55763a372cf1e7a70f6e3c9e0800790e49b5a
VE170618A\admin <shilpi@virtualemployee.com>
2019-01-18 19:50:15 +0530

    route prefix

--- SubjectExample/src/app/app-routing.module.ts
+++ SubjectExample/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { create } from 'domain';
 import {EmployeeCreateComponent} from './employee/employee-create.component';
 import { EmployeeListComponent } from './employee/employee-list.component';
 
 const routes: Routes = [
-  { path:'create', component:EmployeeCreateComponent },
-  { path: 'list' , component: EmployeeListComponent},
-  {path: '', redirectTo:'/list', pathMatch:'full'}
+  {path:'employees', children:[
+    { path: '' , component: EmployeeListComponent},
+    { path:'create', component:EmployeeCreateComponent }    
+    // {path: '', redirectTo:'/list', pathMatch:'full'}
+  ]} 
 ];
 
 @NgModule({
